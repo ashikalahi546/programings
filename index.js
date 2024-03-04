3b7fd3eba85d9fe44253e7e82d0ac4bcaa8b5207
Md.Ashik <ashikalahi546@gmail.com>
2024-03-04 21:35:30 +0600

    updates discuss section

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const discussCard = async (searching = "") => {
   const letsDiscuss = document.getElementById("lets-discuss-section");
   letsDiscuss.innerHTML = "";
   data.posts.forEach((items) => {
-    console.log("data", data);
-
+    // console.log("data", data);
+    //
     const newDiv = document.createElement("div");
     newDiv.classList = `bg-[#12132D0D] px-6 py-8 rounded-2xl `;
     newDiv.innerHTML = ` 
@@ -15,7 +15,8 @@ const discussCard = async (searching = "") => {
     <div class="size-16  bg-white relative">
     <img class="rounded-md" src="${items.image}" alt="">
       <div id = "active"
-        class="size-3 bg-emerald-500 rounded-full absolute -top-1 -right-1"
+        class = "bg-emerald-500 size-3  rounded-full absolute -top-1 -right-1"
+
       > </div>
     </div>
     <div class="flex flex-col gap-3 main-card">
@@ -23,16 +24,14 @@ const discussCard = async (searching = "") => {
         <h3>#${items.category}</h3>
         <h4>Author : ${items.author.name}</h4>
       </div>
-      <h3 class="text-xl font-bold">
+      <h3 class="sm:text-xl text-sm font-bold">
        ${items.title}
       </h3>
       <p class="text-[#12132D99] text-base leading-7"> ${items.description}
-      
       </p>
       <div class="border-y border-dashed "></div>
-
-      <div class="flex sm:gap-48 gap-5 items-center card-option">
-        <div class="flex items-center gap-5">
+      <div class="flex sm:gap-48 gap-2 items-center card-option">
+        <div class="flex items-center sm:gap-5 gap-3">
           <div class="flex items-center gap-2">
             <img src="image/Vector.png" alt="" />
             <p class="text-base text-[#12132D99]">${items.comment_count}</p>
@@ -55,10 +54,11 @@ const discussCard = async (searching = "") => {
       </div>
     </div>
   </div>
-    
+
     `;
     letsDiscuss.appendChild(newDiv);
   });
+
   loadingSpinner(false);
 };
 
@@ -70,15 +70,14 @@ const latestPost = async () => {
   );
   const data = await res.json();
   data.forEach((items) => {
-    console.log(items);
+    // console.log(items);
 
     const latestPosts = document.getElementById("latest-posts-section");
     const newDiv = document.createElement("div");
     newDiv.classList = `shadow-md p-5 rounded-2xl border flex flex-col gap-4`;
-    newDiv.innerHTML = ` 
-
-
-<img class ="rounded-2xl" src="${items.cover_image}" alt="" />
+    newDiv.innerHTML = `<img class ="rounded-2xl" src="${
+      items.cover_image
+    }" alt="" />
             <div class="flex items-center gap-3">
               <img src="image/Frame.png" alt="" />
               <p class="text-[#12132D99]">${
@@ -97,7 +96,6 @@ const latestPost = async () => {
             <img class="rounded-full size-12" src="${
               items.profile_image
             }" alt="">
-          
               <div>
                 <h2 class="font-bold">${items.author.name}</h2>
                 <p class="text-[#12132D99] text-sm">${
@@ -114,7 +112,6 @@ const latestPost = async () => {
 const searchHandler = () => {
   loadingSpinner(true);
   const inputField = document.getElementById("input-field").value;
-
   if (inputField) {
     discussCard(inputField);
   } else {
@@ -131,21 +128,22 @@ const loadingSpinner = (isLoading) => {
   }
 };
 
+const counterElement = document.getElementById("countNumber");
+let countNumber = 0;
+
 const arr = [];
 const emailHandlar = (data) => {
-  const title = data.split("ashik ")[0];
-  const views = data.split("ashik ")[1];
+  const title = data.split("ashik")[0];
+  const views = data.split("ashik")[1];
   arr.push({ title: title, views: views });
-  console.log(arr);
-  arr.forEach((element) => {
-    // console.log(element);
-    const count = document.getElementById("countNumber");
-    const number = count.innerText;
-    if (number) {
-    } else {
-    }
 
-    const tittle = document.getElementById("title-discus");
+  const tittle = document.getElementById("title-discus");
+  tittle.innerHTML = "";
+  console.log(countNumber);
+  countNumber++;
+  counterElement.textContent = `(${countNumber})`;
+
+  arr.forEach((element) => {
     const newdivs = document.createElement("div");
     newdivs.classList = `bg-white shadow p-4 flex items-center xl:gap-5 gap-4 rounded-xl justify-between`;
     newdivs.innerHTML = `
